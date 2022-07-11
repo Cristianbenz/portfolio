9bcd9d5974eded7d192f6e10e5735255d3855a2b
Cristianbenz <contactocbenitez@gmail.com>
2022-07-11 10:00:59 -0300

    error de consola, alt de img

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -4,7 +4,7 @@ import './navBar.css'
 export default function NavBar(){
     return <div className="navBar-position navBar-layout navBar-bg">
                 <Link to={'/'}>
-                    <img src="https://i.ibb.co/QmKjkLm/logo.png" />
+                    <img src="https://i.ibb.co/QmKjkLm/logo.png" alt="logo de cbenz" />
                 </Link>
             </div>
 }
